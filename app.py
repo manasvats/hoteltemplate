2bf66abe3720ded5b9911d620a9ea54b74907623
Manas Vats <123404365+manasvats@users.noreply.github.com>
2026-01-30 22:36:17 +0530

    linking issues fixed css js img

--- app.py
+++ app.py
@@ -6,5 +6,29 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/room')
+def room():
+    return render_template('room.html')
+
+@app.route('/amenities')
+def amenities():
+    return render_template('amenities.html')
+
+@app.route('/booking')
+def booking():
+    return render_template('booking.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
